5a7a084b67757fe7f9c46821b502a83786b7c236
Jiankun Wei <72998341+david-wei-01001@users.noreply.github.com>
2026-08-27 23:04:37 -0400

    Add new functions to task module imports

--- circuit_discovery/tasks/__init__.py
+++ circuit_discovery/tasks/__init__.py
@@ -4,11 +4,17 @@ from .articulatory_index import (
     load_articulatory_dataset,
 )
 from .hubert_classifier import CircuitHubertClassifier, HubertClassificationHead
+from .discovery_setup import (       
+    load_hubert_classifier_for_task,
+    build_particles,
+    finalize_and_report,
+)
 
 __all__ = [
     "TASK_SPECS",
     "prepare_and_save_articulatory_dataset",
     "load_articulatory_dataset",
+    "load_hubert_classifier_for_task",
     "CircuitHubertClassifier",
     "HubertClassificationHead",
 ]
